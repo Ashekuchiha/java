4cec638f9d347a2b68ee7120e7ee8473ee612470
Ashekuchiha <ashakulislam0000@gmail.com>
2023-09-23 12:26:09 +0600

    first commit

---
+++ eclips/test1/src/test1/b.java
@@ -0,0 +1,11 @@
+package test1;
+
+public class b {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("nola");
+
+	}
+
+}
---
+++ eclips/test2/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author shakil
+ *
+ */
+module test2 {
+}
